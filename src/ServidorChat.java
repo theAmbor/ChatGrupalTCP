a8a962af13d5e7525bc22c77ec38b5031c6ae955
theAmbor <sarasanpa03@hotmail.com>
2024-02-29 20:29:59 +0100

    Creamos la clase ManejadorCliente dentro del Servidor para que sea más fácil utilizar las variables y métodos declarados anteriormente, ya que son static.

--- src/ServidorChat.java
+++ src/ServidorChat.java
@@ -8,6 +8,7 @@ public class ServidorChat {
     private static final int PUERTO = 5000;
     private static ArrayList<PrintWriter> flujosSalida = new ArrayList<>();
     private static ArrayList<String> usuariosConectados = new ArrayList<>();
+
     public static void main(String[] args) {
         try {
             ServerSocket serverSocket = new ServerSocket(PUERTO);
@@ -16,10 +17,40 @@ public class ServidorChat {
             while (true) {
                 Socket clienteSocket = serverSocket.accept();
                 System.out.println("Nuevo cliente conectado");
+
+                Thread t = new Thread(new ManejadorCliente(clienteSocket));
+                t.start();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    private static class ManejadorCliente implements Runnable {
+        private Socket clienteSocket;
+        private BufferedReader lector;
+        private PrintWriter escritor;
+        private String nombreUsuario;
+
+        public ManejadorCliente(Socket socket) {
+            try {
+                clienteSocket = socket;
+                lector = new BufferedReader(new InputStreamReader(clienteSocket.getInputStream()));
+                escritor = new PrintWriter(clienteSocket.getOutputStream(), true);
+
+                nombreUsuario = lector.readLine();
+                usuariosConectados.add(nombreUsuario);
+
+                flujosSalida.add(escritor);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void run() {
+
+        }
+    }
 }
+
